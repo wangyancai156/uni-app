9c22012dd5b17cb71d83b406dafe09c9c19a4d8e
wangyancai666 <ycai@jk-sci.com>
2021-04-02 15:24:39 +0800

    初始化

---
+++ pages/contact/contact.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<view>
+			<text> 唱歌跳舞打篮球 </text>	
+		</view>
+		<view>
+			<text selectable space="emsp"> 唱歌	跳舞打篮球 </text>
+		</view>
+		<view>
+			<text selectable="true" space="ensp" style="font-size: larger;"> 唱歌	跳舞打篮球 </text>
+		</view>
+		<view>
+			<text> &nbsp </text>
+		</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ pages/detail/detail.vue
@@ -0,0 +1,35 @@
+<template>
+	<view>详情页
+		<text> 唱歌跳舞打篮球 </text>	
+		<text selectable > 唱歌跳舞打篮球 </text>
+		<view>
+			<text> 唱歌跳舞打篮球 </text>	
+			<text selectable space="ensp" > 唱歌     跳舞打篮球 </text>
+		</view>
+		<view>
+			<text decode="false"> &amp; </text>	
+		</view>
+		
+		<view class="box" hover-class="box-active">
+			我是一个大盒子
+		</view>
+	</view>
+</template>
+
+<script> 
+</script>
+
+<style>
+	
+	.box{
+		width: 100px;
+		height: 100px;
+		background: green;
+	}
+	
+	.box-active{
+		width: 100px;
+		height: 100px;
+		background: red;
+	}
+</style>
